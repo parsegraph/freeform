8e475bcda5ca3a601f2809556a6dde4bd020ebb5
Aaron Faanes <dafrito@gmail.com>
2024-02-18 22:53:04 -0600

    add license

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import './App.css';
 
 import { 
   Direction,
+  DirectionNode,
   CommitLayout,
   paintNodeBounds,
   paintNodeLines,
   Alignment,
+  DirectionCaret,
+  Axis,
 } from "parsegraph";
 import Color from 'parsegraph-color';
 import { BasicGLProvider } from 'parsegraph-compileprogram';
@@ -18,35 +21,65 @@ import {
   makeScale3x3
 } from 'parsegraph-matrix';
 
-import PrimesWidget from './PrimesWidget';
-
 const fontSize = 36;
+const borderThickness = 3;
+const borderRoundedness = 5;
+const maxClickDelay = 1000;
 
 function App() {
   const canvasRef = useRef();
 
   useEffect(() => {
     if (!canvasRef.current) {
+      // No canvas yet.
       return;
     }
+
+    // Remove all prior children.
     while (canvasRef.current.firstChild) {
       canvasRef.current.firstChild.remove();
     }
 
+    // Create and restore the camera if possible
     const cam = new Camera();
+    try {
+      const camData = localStorage.getItem("parsegraph-camera");
+      if (camData) {
+        cam.restore(JSON.parse(camData))
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
 
-    // Render the graph.
-    let isDown = false;
+    // Build the graph
+    const widget = new DirectionNode("hey it widget root node");
+    widget.connect(Direction.FORWARD, new DirectionNode("its forward node"));
+    widget.neighbors().nodeAt(Direction.FORWARD).connect(Direction.FORWARD, new DirectionNode("its another forward node"));
+
+    // Input event callbacks
+    let isDown = null;
     const canvas = canvasRef.current;
-
+    canvas.tabIndex = '0';
     let [mouseX, mouseY] = [NaN, NaN];
+    let userCaret = new DirectionCaret(widget);
+    let showEditor = false;
     canvas.addEventListener('mousedown', e => {
-      isDown = true;
+      isDown = Date.now();
       [mouseX, mouseY] = [e.clientX, e.clientY];
       refresh();
     });
     canvas.addEventListener('mouseup', e => {
-      isDown = false;
+      if (!isNaN(isDown) && Date.now() - isDown < maxClickDelay) {
+        const size = [0, 0];
+        const [worldX, worldY] = cam.transform(mouseX, mouseY);
+        let selectedNode = widget.layout().nodeUnderCoords(worldX, worldY, 1, size);
+        if (selectedNode) {
+          console.log(selectedNode);
+          userCaret.moveTo(selectedNode);
+          refresh();
+        }
+      }
+      isDown = null;
       [mouseX, mouseY] = [NaN, NaN];
       refresh();
     });
@@ -60,11 +93,91 @@ function App() {
       [mouseX, mouseY] = [e.clientX, e.clientY];
     });
     canvas.addEventListener('wheel', e => {
-      console.log(e);
       if (!isNaN(mouseX)) {
         cam.zoomToPoint(Math.pow(1.1, e.deltaY > 0 ? -1 : 1), mouseX, mouseY);
         refresh();
       }
+    });
+    canvas.addEventListener('keydown', e => {
+      if (showEditor) {
+        return;
+      }
+      const spawnMove = (dir) => {
+        if (userCaret.node().neighbors().hasNode(dir)) {
+          userCaret.move(dir);
+          refresh();
+        } else {
+          userCaret.spawnMove(dir);
+          refresh();
+        }
+      }
+      console.log(e);
+      switch (e.key) {
+        case 'x':
+        case 'Backspace':
+          if (userCaret.node().neighbors().isRoot()) {
+            return;
+          }
+          const node = userCaret.node();
+          userCaret.moveTo(node.neighbors().parentNode());
+          node.disconnect();
+          refresh();
+          break;
+        case 'o':
+          if (userCaret.has(Direction.OUTWARD)) {
+            userCaret.move(Direction.OUTWARD);
+            refresh();
+          }
+          break;
+        case 'i':
+          spawnMove(Direction.INWARD);
+          break;
+        case 'ArrowDown':
+        case 'j':
+          spawnMove(Direction.DOWNWARD);
+          break;
+        case 'ArrowUp':
+        case 'k':
+          spawnMove(Direction.UPWARD);
+          break;
+        case 'ArrowRight':
+        case 'l':
+          spawnMove(Direction.FORWARD);
+          break;
+        case 'ArrowLeft':
+        case 'h':
+          spawnMove(Direction.BACKWARD);
+          break;
+        case '`':
+        case '~':
+          if (userCaret.node().scale() !== 1) {
+            userCaret.node().setScale(1);
+          } else {
+            userCaret.shrink();
+          }
+          refresh();
+          break;
+        case 'Enter':
+          showEditor = !showEditor;
+          if (showEditor) {
+            editor.style.display = 'block';
+            editor.focus();
+            const val = userCaret.node().value();
+            if (typeof val === "string" && val !== '') {
+              editor.value = userCaret.node().value();
+            } else {
+              editor.value = '';
+            }
+          } else {
+            editor.style.display = 'none';
+          }
+          refresh();
+          break;
+        default:
+          console.log(e);
+          return;
+      }
+      e.preventDefault();
     });
 
     new ResizeObserver(() => {
@@ -72,8 +185,6 @@ function App() {
       refresh();
     }).observe(canvas);
 
-    canvas.style.background = 'black';
-    canvas.style.overflow = 'hidden';
 
     const refresh = () => {
       requestAnimationFrame(() => {
@@ -81,9 +192,8 @@ function App() {
       });
     };
 
-    const widget = new PrimesWidget();
-
-    const glProvider = new BasicGLProvider();
+    canvas.style.background = 'black';
+    canvas.style.overflow = 'hidden';
 
     const textCanvas = document.createElement('canvas');
     textCanvas.style.position = 'absolute';
@@ -93,6 +203,7 @@ function App() {
     textCanvas.style.bottom = 0;
     const ctx = textCanvas.getContext("2d");
 
+    const glProvider = new BasicGLProvider();
     glProvider.container();
     glProvider.container().style.position = 'relative';
     glProvider.container().style.width = '100%';
@@ -100,17 +211,71 @@ function App() {
     canvas.appendChild(glProvider.container());
     canvas.appendChild(textCanvas);
 
+    const editor = document.createElement('input');
+    editor.style.position = 'absolute';
+    editor.style.bottom = '32px';
+    editor.style.left = '25%';
+    editor.style.right = '25%';
+    editor.style.fontSize = '24px';
+    editor.style.display = 'none';
+    editor.addEventListener('keypress', e => {
+      if (e.key === 'Escape') {
+        showEditor = false;
+        editor.style.display = 'none';
+        canvas.focus();
+      } else if (e.key === 'Enter') {
+        showEditor = false;
+        userCaret.node().setValue(editor.value);
+        editor.style.display = 'none';
+        canvas.focus();
+        refresh();
+      }
+    })
+    canvas.appendChild(editor);
+
     const painters = new WeakMap();
 
     let layout;
     const createLayout = () => {
-      return new CommitLayout(widget.node(), {
+      return new CommitLayout(widget, {
         size: (node, size) => {
           size[0] = fontSize;
+          const hasValue = typeof node.value() === "string" || typeof node.value() === "number";
+          if (hasValue) {
+            const { width } = ctx.measureText(node.value());
+            size[0] = Math.max(size[0], width + 6*borderThickness);
+          }
           size[1] = fontSize;
+
+          if (node.neighbors().hasNode(Direction.INWARD)) {
+            const child = node.neighbors().nodeAt(Direction.INWARD);
+            const childSize = [0, 0];
+            child.layout().extentSize(childSize);
+
+            if (node.neighbors().getAlignment(Direction.INWARD) === Alignment.INWARD_VERTICAL) {
+              if (!hasValue) {
+                size[0] = 4*borderThickness;
+                size[1] = 4*borderThickness;
+              }
+              // Vertically aligned inward node.
+              size[0] = Math.max(size[0], 2*borderThickness + childSize[0]);
+              size[1] += childSize[1];
+            } else {
+              if (!hasValue) {
+                size[0] = borderThickness;
+                size[1] = borderThickness;
+              }
+              // Default is horizontal
+              size[0] += childSize[0];
+              size[1] = Math.max(size[1], borderThickness + childSize[1]);
+            }
+          }
         },
-        getSeparation: () => {
-          return 3;
+        getSeparation: (node, axis) => {
+          if (axis === Axis.Z) {
+            return borderThickness;
+          }
+          return fontSize/2;
         },
         paint: (pg) => {
           let painter = painters.get(pg);
@@ -125,7 +290,7 @@ function App() {
 
           let numBlocks = 0;
           pg.siblings().forEachNode(node => {
-            paintNodeLines(node, 1, () => {
+            paintNodeLines(node, borderThickness, () => {
               ++numBlocks;
             });
             paintNodeBounds(node, () => {
@@ -136,7 +301,7 @@ function App() {
           painter.initBuffer(numBlocks);
 
           pg.siblings().forEachNode(node => {
-            paintNodeLines(node, 1, (x, y, w, h) => {
+            paintNodeLines(node, borderThickness/2, (x, y, w, h) => {
               painter.setBackgroundColor(new Color(0.5, 0.5, 0.5, 0.5));
               painter.setBorderColor(new Color(1, 1, 1, 1));
               painter.drawBlock(x, y, w, h, 0, 0);
@@ -150,20 +315,20 @@ function App() {
                 painter.setBorderColor(new Color(1, 1, 1, 1));
               }
               else {
-                painter.setBackgroundColor(new Color(0, 0, 1, 1));
+                painter.setBackgroundColor(new Color(0, 0, 1, .1));
                 painter.setBorderColor(new Color(1, 1, 1, 1));
               }
               const scale = node.layout().groupScale();
-              painter.drawBlock(x, y, w, h, 5 * scale, 3 * scale);
+              painter.drawBlock(x, y, w, h, borderRoundedness * scale, borderThickness * scale);
             });
           });
         }
       });
     }
 
-    let timer;
     const paint = () => {
-      if (!layout || layout.startingNode() !== widget.node()) {
+      localStorage.setItem("parsegraph-camera", JSON.stringify(cam.toJSON()))
+      if (!layout || layout.startingNode() !== widget) {
         console.log("Creating new layout");
         layout = createLayout();
       }
@@ -184,19 +349,6 @@ function App() {
       }*/
 
       render();
-      const MAX_PRIME = 180;
-      if (widget.position <= MAX_PRIME) {
-        if (timer) {
-          clearTimeout(timer);
-        }
-        timer = setTimeout(() => {
-          let start = Date.now();
-          while (Date.now() - start < 10 && widget.position <= MAX_PRIME) {
-            widget.step();
-          }
-          refresh();
-        }, 500)
-      }
     };
 
     const render = () => {
@@ -222,7 +374,7 @@ function App() {
       ctx.scale(cam.scale(), cam.scale());
       ctx.translate(cam.x(), cam.y());
 
-      let pg = widget.node();
+      let pg = widget;
       do {
         const painter = painters.get(pg);
         if (!painter) {
@@ -258,14 +410,14 @@ function App() {
             const nodeSize = [0, 0]
             node.layout().size(nodeSize);
             const scale = node.layout().groupScale();
-            if (node.neighbors().getAlignment(Direction.INWARD) === Alignment.INWARD_HORIZONTAL) {
-              ctx.textAlign = 'left';
-              ctx.textBaseline = 'middle';
-              ctx.translate(node.layout().groupX() - scale * nodeSize[0]/2 + scale * 3/2, node.layout().groupY());
-            } else {
+            if (node.neighbors().getAlignment(Direction.INWARD) === Alignment.INWARD_VERTICAL) {
               ctx.textAlign = 'center';
               ctx.textBaseline = 'top';
               ctx.translate(node.layout().groupX(), node.layout().groupY() - scale * nodeSize[1]/2 + scale * 3);
+            } else {
+              ctx.textAlign = 'left';
+              ctx.textBaseline = 'middle';
+              ctx.translate(node.layout().groupX() - scale * nodeSize[0]/2 + scale * 3/2, node.layout().groupY());
             }
           } else {
             ctx.textAlign = 'center';
@@ -273,14 +425,37 @@ function App() {
             ctx.translate(node.layout().groupX(), node.layout().groupY());
           }
           ctx.scale(node.layout().groupScale(), node.layout().groupScale());
-              console.log(ctx.textAlign, ctx.textBaseline);
           ctx.fillText(node.value(), 0, 0)
           ctx.restore();
         });
         ctx.restore();
 
         pg = pg.paintGroup().next();
-      } while (pg !== widget.node());
+      } while (pg !== widget);
+
+      if (userCaret.node()) {
+        const layout = userCaret.node().layout();
+        /*ctx.fillStyle = 'none';
+        ctx.strokeStyle = 'yellow';
+        const extentSize = [0, 0];
+        layout.extentSize(extentSize);
+        ctx.strokeRect(
+          layout.absoluteX() - layout.extentOffsetAt(Direction.DOWNWARD),
+          layout.absoluteY() - layout.extentOffsetAt(Direction.FORWARD), extentSize[0], extentSize[1]
+        );*/
+        ctx.strokeStyle = 'orange';
+        const bodySize = [0, 0];
+        layout.size(bodySize);
+        ctx.strokeRect(
+          layout.absoluteX() - layout.absoluteScale() * bodySize[0]/2,
+          layout.absoluteY() - layout.absoluteScale() * bodySize[1]/2, layout.absoluteScale() * bodySize[0], layout.absoluteScale() * bodySize[1]
+        );
+
+        ctx.resetTransform();
+        ctx.textAlign = 'left';
+        ctx.textBaseline = 'top';
+        ctx.fillText(userCaret.node()?.value(), 0, 0);
+      }
     };
 
     paint();

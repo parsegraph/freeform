b318dd95535f37f3dee9ab4ec9c07ccac6ac5cd1
Aaron Faanes <dafrito@gmail.com>
2024-03-09 16:20:16 -0600

    Include static

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -3,7 +3,10 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
-    asar: true
+    asar: true,
+    extraResources: [
+      "./static"
+    ]
   },
   rebuildConfig: {},
   makers: [

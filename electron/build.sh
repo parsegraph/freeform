56dd6b339b7a86a9c8b3b438bc1ad091f5cb100e
Aaron Faanes <dafrito@gmail.com>
2024-03-09 16:31:20 -0600

    Fix non-windows builds

--- electron/build.sh
+++ electron/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd ../occluder && ./deploy.sh && cd -
-cd .. && yarn build && cd -
+cd ../occluder && yarn && ./deploy.sh && cd -
+cd .. && yarn && yarn build && cd -
 cp -f ../build/index.html ../build/favicon.ico .
 cp -r -f ../build/static .
+sed -i -re 's/React App/parsegraph/g' ./index.html
 yarn make
